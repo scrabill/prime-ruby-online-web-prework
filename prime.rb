8327165eabf358bd2b921475008e0910e665b98d
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-07 14:22:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,51 @@
 # Add  code here!
+
+number = 8
+
+range = 1...number
+
+def prime?(range,number)
+  range.each{|x| return number % x}
+
+  if return_of_x == 0
+    return false
+  else
+    return true
+  end
+end
+
+### OG WORK
+
+#def prime?(number)
+
+  #range = (1..number)
+
+  #range.each do |range|
+    #if number % range == 0
+    #  false
+    #else
+    #  true
+    #end
+  #end
+
+
+  #counter = number - 1
+
+  #while counter >= number
+    #if number % counter = 0
+      #counter -= 1
+      #false
+    #else
+      #true
+    #end
+
+#  end
+
+  #basket.each do |apple|
+  #puts "Taking out #{apple}"
+  #end
+
+  # A naive solution is to iterate through all numbers from 2 to n-1 and for every number check if it divides n. If we find any number that divides, we return false.
+#end
+
+prime?(range,number)
