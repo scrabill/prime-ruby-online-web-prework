0c3ba3d617c643fad24c407c50cd31081969af5c
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-07 14:46:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,6 @@
 
 require "pry"
 
-number = 8
-
 
 
 def prime?(number)
@@ -53,5 +51,7 @@ end
 
   # A naive solution is to iterate through all numbers from 2 to n-1 and for every number check if it divides n. If we find any number that divides, we return false.
 #end
+
+number = 8
 
 prime?(range,number)
